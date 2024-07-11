b2dc7319b9b373b826c6ad021d8a07795c93df16
Zeezbaba <Azeez_okeleji>
2024-07-11 20:21:00 +0000

    StarWars

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,34 +1,17 @@
 #!/usr/bin/node
 
 const request = require('request');
-const baseUrl = 'https://swapi.dev/api/films/';
 
-// Function to get the characters of a movie
-function getMovieCharacters(movieId) {
-  // Fetch the movie details from the Star Wars API
-  request(`${baseUrl}${movieId}/`, (error, response, body) => {
-    if (!error && response.statusCode == 200) {
-      const movieData = JSON.parse(body);
-      const characters = movieData.characters;
-
-      // Function to fetch character names sequentially
-      function fetchCharacterName(url, callback) {
-        request(url, (error, response, body) => {
-          if (!error && response.statusCode == 200) {
-            const characterData = JSON.parse(body);
-            callback(characterData.name);
-          }
-        });
-      }
-
-      // Loop through each character URL and fetch the name
-      characters.forEach((characterUrl) => {
-        fetchCharacterName(characterUrl, (name) => {
-          console.log(name);
-        });
-      });
-    } else {
-      console.log(`Failed to fetch movie details for ID ${movieId}`);
-    }
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, res, body) {
+  if (err) throw err;
+  const actors = JSON.parse(body).characters;
+  exactOrder(actors, 0);
+});
+const exactOrder = (actors, x) => {
+  if (x === actors.length) return;
+  request(actors[x], function (err, res, body) {
+    if (err) throw err;
+    console.log(JSON.parse(body).name);
+    exactOrder(actors, x + 1);
   });
-}
+};
